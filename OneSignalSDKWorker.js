8a4cb6a373ee47689339f671ef192febd85ef0fc
Justin Wayne <justin@onesignal.com>
2024-08-21 10:21:31 -0500

    now with serviceWorker

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,5 @@
-importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+// Prod:
+// importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
+
+// Staging:
+importScripts('https://staging.onesignal.com/sdks/Staging-OneSignalSDKWorker.js');
